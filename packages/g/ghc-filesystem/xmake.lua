565712c6920036e46a5bb017dd7c20bf24e7d558
zeromake <a390720046@gmail.com>
2022-12-20 19:52:10 +0800

    feat: add ghc-filesystem pkg

---
+++ packages/g/ghc-filesystem/xmake.lua
@@ -0,0 +1,29 @@
+package("ghc-filesystem")
+    set_kind("library", {headeronly = true})
+    set_homepage("https://github.com/gulrak/filesystem")
+    set_description("An implementation of C++17 std::filesystem for C++11 /C++14/C++17/C++20 on Windows, macOS, Linux and FreeBSD.")
+    set_license("MIT")
+    set_urls("https://github.com/gulrak/filesystem/archive/refs/tags/v1.5.12.tar.gz")
+    
+    add_versions("1.5.12", "7d62c5746c724d28da216d9e11827ba4e573df15ef40720292827a4dfd33f2e9")
+
+    on_install("windows", "mingw", "macosx", "linux", "iphoneos", "android", function (package)
+        io.writefile("xmake.lua", [[
+            add_rules("mode.debug", "mode.release")
+            target("ghc-filesystem")
+                set_kind("headeronly")
+                add_headerfiles("include/ghc/*.hpp", {prefixdir = "ghc"})
+        ]])
+        local configs = {}
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets([[
+namespace fs = ghc::filesystem;
+void test() {
+    fs::path dir(".");
+    for (auto f : fs::directory_iterator(dir)) {
+    }
+}]], {includes = {"ghc/filesystem.hpp"}, configs = {languages = "c++17"}}))
+    end)
