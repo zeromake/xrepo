764276e7f9fcd5088da6f08d7ca3389a74148ef4
zeromake <a390720046@gmail.com>
2023-02-16 10:57:05 +0800

    fix: tweeny include string

--- packages/t/tweeny/xmake.lua
+++ packages/t/tweeny/xmake.lua
@@ -7,7 +7,8 @@ package("tweeny")
     
     add_versions("3.2.0", "bdb8ad985a1d7ee30a3f441d67030f55de47d1961b71a0e2c3062df3c1d80fa3")
     on_install("windows", "mingw", "macosx", "linux", "iphoneos", "android", function (package)
-        os.cp("../tweeny-"..package:version()..".h", path.join(package:installdir("include"), "tweeny.h"))
+        local content = io.readfile("../tweeny-"..package:version()..".h"):gsub("namespace tweeny", "#include <string>\n\nnamespace tweeny")
+        io.writefile(path.join(package:installdir("include"), "tweeny.h"), content);
     end)
 
     -- on_test(function (package)
