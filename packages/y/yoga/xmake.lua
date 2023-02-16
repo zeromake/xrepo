cb6f69c8ce63a8ac09595df410cf283037111278
zeromake <a390720046@gmail.com>
2023-02-16 13:55:41 +0800

    fix: yoga use main latest

--- packages/y/yoga/xmake.lua
+++ packages/y/yoga/xmake.lua
@@ -2,9 +2,10 @@ package("yoga")
     set_homepage("https://yogalayout.com")
     set_description("Yoga is a cross-platform layout engine which implements Flexbox.")
     set_license("MIT")
-    set_urls("https://github.com/facebook/yoga/archive/refs/tags/v$(version).tar.gz")
-    
-    add_versions("1.19.0", "65a1b40390ab434a376cd8a60c4ab624185c982b5ed793c9d59111450e0b92d4")
+    set_urls(
+        "https://github.com/facebook/yoga/archive/220d2582c94517b59d1c36f1c2faf5e3f88306f1.zip"
+    )
+    add_versions("latest", "a4477216404e803ba91adfdf672339e2e0ca803ec104a0b31354fed066e5ed46")
     on_install("windows", "mingw", "macosx", "linux", "iphoneos", "android", function (package)
         os.cp(path.join(os.scriptdir(), "port", "xmake.lua"), "xmake.lua")
         local configs = {}
