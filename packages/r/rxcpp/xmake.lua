7b8ce07fc5afa6f3430f0f529af3fa8ac53a4b09
zeromake <a390720046@gmail.com>
2023-03-11 15:10:14 +0800

    feat: add rxcpp pkg

---
+++ packages/r/rxcpp/xmake.lua
@@ -0,0 +1,14 @@
+package("rxcpp")
+    set_homepage("https://github.com/ReactiveX/RxCpp")
+    set_description("Reactive Extensions for C++")
+    set_license("MIT")
+    set_urls("https://github.com/ReactiveX/RxCpp/archive/refs/tags/v$(version).tar.gz")
+    
+    add_versions("4.1.1", "054a9be63e66904ecaa552ce56d6458b4545d6da5ea32f5103c7fc379cebb374")
+    on_install("windows", "mingw", "macosx", "linux", "iphoneos", "android", function (package)
+        os.cp("Rx/v2/src/rxcpp", package:installdir("include").."/")
+    end)
+
+    -- on_test(function (package)
+    --     assert(package:has_cfuncs("xxx", {includes = {"xx.h"}}))
+    -- end)
