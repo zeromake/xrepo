9e29cfea1688434c8c6449062ec809e0465f390c
zeromake <a390720046@gmail.com>
2023-05-23 09:41:46 +0800

    fix: qr code add c++11

--- packages/q/qr-code-generator/port/xmake.lua
+++ packages/q/qr-code-generator/port/xmake.lua
@@ -2,6 +2,9 @@ add_rules("mode.debug", "mode.release")
 
 if is_plat("windows") then
     add_cxflags("/utf-8")
+    set_languages("c++17")
+else
+    set_languages("c++11")
 end
 
 target("qr-code-generator-c")
