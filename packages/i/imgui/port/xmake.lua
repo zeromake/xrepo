866cfa597309a2f25b29cb82268af4e2c53b668c
zeromake <a390720046@gmail.com>
2023-10-09 21:12:17 +0800

    fix: imgui add glfw support

--- packages/i/imgui/port/xmake.lua
+++ packages/i/imgui/port/xmake.lua
@@ -20,23 +20,51 @@ if get_config("freetype") then
     add_requires("freetype")
 end
 
+function string.split(input, delimiter)
+    if (delimiter == "") then return false end
+    local pos, arr = 0, {}
+    for st, sp in function() return string.find(input, delimiter, pos, true) end do
+        table.insert(arr, string.sub(input, pos, st - 1))
+        pos = sp + 1
+    end
+    table.insert(arr, string.sub(input, pos))
+    return arr
+end
+
+local packages = {
+    sdl2="sdl2",
+    sdlrenderer="sdl2",
+    glfw="glfw",
+    glut="freeglut"
+}
+
+local backend = get_config("backend")
+if backend and backend ~= "" then
+    local backends = string.split(backend, ";")
+    for _, backend in ipairs(backends) do
+        if packages[backend] ~= nil then
+            add_requires(backend)
+        end
+    end
+end
+
 target("imgui")
     set_kind("$(kind)")
     add_files("*.cpp|imgui_demo.cpp")
     add_includedirs(".")
-    -- if is_plat("windows", "mingw") then
-    --     add_files("backends/imgui_impl_win32.cpp")
-    -- elseif is_plat("macosx") then
-    --     add_files("backends/imgui_impl_osx.mm")
-    -- elseif is_plat("android") then
-    --     add_files("backends/imgui_impl_android.cpp")
-    -- end
     local backend = get_config("backend")
     if backend and backend ~= "" then
-        if backend == "metal" then
-            add_files("backends/imgui_impl_metal.mm")
-        else
-            add_files("backends/imgui_impl_"..backend..".cpp")
+        local backends = string.split(backend, ";")
+        for _, backend in ipairs(backends) do
+            if packages[backend] ~= nil then
+                add_packages(backend)
+            end
+            add_headerfiles("backends/imgui_impl_"..backend..".h", {prefixdir = "backends"})
+            if backend == "metal" or backend == "osx" then
+                add_files("backends/imgui_impl_"..backend..".mm")
+            else
+                add_files("backends/imgui_impl_"..backend..".cpp")
+            end
         end
     end
     if get_config("freetype") then
