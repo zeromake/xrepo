a6645dd67c3955afc8e3797b95eeaf95ac64bf55
zeromake <a390720046@gmail.com>
2023-10-15 14:01:37 +0800

    feat: add sdl12_compat

---
+++ packages/s/sdl12_compat/port/xmake.lua
@@ -0,0 +1,13 @@
+add_rules("mode.debug", "mode.release")
+
+if is_plat("windows") then
+    add_cxflags("/utf-8")
+end
+
+add_requires("sdl2")
+
+target("sdl12_compat")
+    set_kind("$(kind)")
+    add_packages("sdl2")
+    add_files("src/SDL12_compat.c")
+    add_headerfiles(path.join("include", "*.h"), {prefixdir="SDL2"})
---
+++ packages/s/sdl12_compat/xmake.lua
@@ -0,0 +1,19 @@
+package("sdl12_compat")
+    set_homepage("https://github.com/libsdl-org/sdl12-compat")
+    set_description("An SDL-1.2 compatibility layer that uses SDL 2.0 behind the scenes.")
+    set_license("MIT")
+    set_urls("https://github.com/libsdl-org/sdl12-compat/archive/refs/tags/release-$(version).tar.gz")
+
+    add_versions("1.2.68", "63c6e4dcc1154299e6f363c872900be7f3dcb3e42b9f8f57e05442ec3d89d02d")
+    add_deps("sdl2")
+    add_includedirs("include")
+    add_includedirs("include/SDL2")
+    on_install(function (package)
+        os.cp(path.join(os.scriptdir(), "port", "xmake.lua"), "xmake.lua")
+        local configs = {}
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    -- on_test(function (package)
+    --     assert(package:has_cfuncs("xxx", {includes = {"xx.h"}}))
+    -- end)
