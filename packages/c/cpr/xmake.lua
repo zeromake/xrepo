e1dbc193b672af413b50e617ca05612ac863dfbb
zeromake <a390720046@gmail.com>
2023-11-02 23:55:40 +0800

    feat: cpr use xmake build

--- packages/c/cpr/xmake.lua
+++ packages/c/cpr/xmake.lua
@@ -6,47 +6,36 @@ package("cpr")
     set_urls("https://github.com/libcpr/cpr/archive/refs/tags/$(version).tar.gz")
 
     add_versions("1.10.5", "c8590568996cea918d7cf7ec6845d954b9b95ab2c4980b365f582a665dea08d8")
-    add_versions("1.10.0", "d669c028bd63a1c8827c32b348ecc85e46747bb33be3b00ce59b77717b91aee8")
-    add_versions("1.8.3", "0784d4c2dbb93a0d3009820b7858976424c56578ce23dcd89d06a1d0bf5fd8e2")
-    add_configs("ssl", {description = "Enable SSL.", default = false, type = "boolean"})
 
-    local curlName = "curl"
-    add_deps("cmake")
-    if is_plat("mingw") then
-        add_syslinks("pthread")
-        curlName = "libcurl"
-    end
     add_links("cpr")
-
-    on_load(function (package)
-        package:add("deps", "zlib")
-        if package:config("ssl") then
-            package:add("deps", curlName)
-            package:add("deps", "libssh2")
-        else
-            package:add("deps", curlName)
-        end
-    end)
+    add_deps("curl")
 
     on_install(function (package)
-        local configs = {"-DCPR_BUILD_TESTS=OFF",
-                         "-DCPR_USE_SYSTEM_CURL=ON"}
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        table.insert(configs, "-DCPR_ENABLE_SSL=" .. (package:config("ssl") and "ON" or "OFF"))
-        local shflags
-        if package:config("shared") and package:is_plat("macosx") then
-            shflags = {"-framework", "CoreFoundation", "-framework", "Security", "-framework", "SystemConfiguration"}
-        end
-        local packagedeps = {curlName, "zlib"}
-        if package:config("ssl") then
-            table.insert(packagedeps, "libssh2")
-        end
-        if package:is_plat("windows", "mingw") then
-            -- fix find_package issue on windows
-            io.replace("CMakeLists.txt", "find_package%(CURL COMPONENTS .-%)", "find_package(CURL)")
-        end
-        import("package.tools.cmake").install(package, configs, {shflags = shflags, packagedeps = packagedeps})
+        io.writefile("include/cpr/cprver.h", [[
+#ifndef CPR_CPRVER_H
+#define CPR_CPRVER_H
+#define CPR_VERSION "1.10.5"
+#define CPR_VERSION_MAJOR 1
+#define CPR_VERSION_MINOR 10
+#define CPR_VERSION_PATCH 5
+#define CPR_VERSION_NUM 0x011005
+#endif
+]])
+        io.writefile("xmake.lua", [[
+add_rules("mode.debug", "mode.release")
+
+add_requires("curl", {system=false})
+
+set_languages("c++17")
+
+target("cpr")
+    set_kind("$(kind)")
+    add_packages("curl")
+    add_includedirs("include")
+    add_headerfiles("include/cpr/*.h", {prefixdir = "cpr"})
+    add_files("cpr/*.cpp")
+]])
+        import("package.tools.xmake").install(package, configs)
     end)
 
     on_test(function (package)
