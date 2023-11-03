b94caf39bd7388139e95dabedd0d170cbcb8f6fb
zeromake <a390720046@gmail.com>
2023-11-03 19:22:26 +0800

    feat: update mbedtls to 3.5.0

--- packages/m/mbedtls/port/xmake.lua
+++ packages/m/mbedtls/port/xmake.lua
@@ -1,138 +1,17 @@
-if xmake.version():gt("2.8.3") then
-    includes("@builtin/check")
-else
-    includes("check_cincludes.lua")
-end
 add_rules("mode.debug", "mode.release")
-
-local options = {}
-
-for _, op in ipairs(options) do
-    option(op)
-        set_default(false)
-        set_showmenu(true)
-    option_end()
-    if has_config(op) then 
-        add_requires(op, {system=false})
-    end
-end
 
 if is_plat("windows") then
     add_cxflags("/utf-8")
 end
-
-local sourceFiles = {
-    "aes.c",
-    "aesni.c",
-    "aria.c",
-    "asn1parse.c",
-    "asn1write.c",
-    "base64.c",
-    "bignum.c",
-    "camellia.c",
-    "ccm.c",
-    "chacha20.c",
-    "chachapoly.c",
-    "cipher.c",
-    "cipher_wrap.c",
-    "constant_time.c",
-    "cmac.c",
-    "ctr_drbg.c",
-    "des.c",
-    "dhm.c",
-    "ecdh.c",
-    "ecdsa.c",
-    "ecjpake.c",
-    "ecp.c",
-    "ecp_curves.c",
-    "entropy.c",
-    "entropy_poll.c",
-    "error.c",
-    "gcm.c",
-    "hkdf.c",
-    "hmac_drbg.c",
-    "md.c",
-    "md5.c",
-    "memory_buffer_alloc.c",
-    "mps_reader.c",
-    "mps_trace.c",
-    "nist_kw.c",
-    "oid.c",
-    "padlock.c",
-    "pem.c",
-    "pk.c",
-    "pk_wrap.c",
-    "pkcs12.c",
-    "pkcs5.c",
-    "pkparse.c",
-    "pkwrite.c",
-    "platform.c",
-    "platform_util.c",
-    "poly1305.c",
-    "psa_crypto.c",
-    "psa_crypto_aead.c",
-    "psa_crypto_cipher.c",
-    "psa_crypto_client.c",
-    "psa_crypto_driver_wrappers.c",
-    "psa_crypto_ecp.c",
-    "psa_crypto_hash.c",
-    "psa_crypto_mac.c",
-    "psa_crypto_rsa.c",
-    "psa_crypto_se.c",
-    "psa_crypto_slot_management.c",
-    "psa_crypto_storage.c",
-    "psa_its_file.c",
-    "ripemd160.c",
-    "rsa.c",
-    "rsa_alt_helpers.c",
-    "sha1.c",
-    "sha256.c",
-    "sha512.c",
-    "ssl_debug_helpers_generated.c",
-    "threading.c",
-    "timing.c",
-    "version.c",
-    "version_features.c",
-
-    "x509.c",
-    "x509_create.c",
-    "x509_crl.c",
-    "x509_crt.c",
-    "x509_csr.c",
-    "x509write_crt.c",
-    "x509write_csr.c",
-
-    "debug.c",
-    "net_sockets.c",
-    "ssl_cache.c",
-    "ssl_ciphersuites.c",
-    "ssl_client.c",
-    "ssl_cookie.c",
-    "ssl_msg.c",
-    "ssl_ticket.c",
-    "ssl_tls.c",
-    "ssl_tls12_client.c",
-    "ssl_tls12_server.c",
-    "ssl_tls13_keys.c",
-    "ssl_tls13_server.c",
-    "ssl_tls13_client.c",
-    "ssl_tls13_generic.c",
-}
 
 target("mbedtls")
     set_kind("$(kind)")
 
     add_headerfiles("include/mbedtls/*.h", {prefixdir = "mbedtls"})
     add_headerfiles("include/psa/*.h", {prefixdir = "psa"})
-
     add_includedirs("library", "include")
-
-    for _, op in ipairs(options) do
-        if has_config(op) then
-            add_packages(op)
-        end
-    end
-
-    for _, f in ipairs(sourceFiles) do
-        add_files(path.join("library", f))
-    end
+    add_files("library/*.c")
+    add_includedirs("3rdparty/everest/include")
+    add_includedirs("3rdparty/everest/include/everest")
+    add_files("3rdparty/p256-m/**.c")
+    add_files("3rdparty/everest/library/*.c")
--- packages/m/mbedtls/xmake.lua
+++ packages/m/mbedtls/xmake.lua
@@ -1,39 +1,20 @@
-local options = {}
-
 package("mbedtls")
     set_homepage("https://www.trustedfirmware.org/projects/mbed-tls/")
     set_description("An open source, portable, easy to use, readable and flexible TLS library, and reference implementation of the PSA Cryptography API.")
     set_license("Apache-2.0")
-    set_urls("https://github.com/Mbed-TLS/mbedtls/archive/refs/tags/v$(version).tar.gz")
+    set_urls("https://github.com/Mbed-TLS/mbedtls/archive/refs/tags/mbedtls-$(version).tar.gz")
 
-    add_versions("3.2.1", "d0e77a020f69ad558efc660d3106481b75bd3056d6301c31564e04a0faae88cc")
-
-    for _, op in ipairs(options) do
-        add_configs(op, {description = "Support "..op, default = false, type = "boolean"})
-    end
+    add_versions("3.5.0", "02311fc8bd032d89ff9aee535dddb55458108dc0d4c5280638fc611aea7c5e4a")
 
     add_links("mbedtls")
 
-    on_load(function (package)
-        for _, op in ipairs(options) do
-            if package:config(op) then
-                package:add("deps", op)
-            end
-        end
-    end)
-
     on_install(function (package)
-        import("lib.detect.find_program")
+        -- import("lib.detect.find_tool")
+        -- local python = assert(find_tool("python3"), "python not found!")
+        -- os.vrunv(python.program, {"-m", "pip", "install", "--user", "-r", "scripts/driver.requirements.txt"})
+        -- os.vrunv(python.program, {"scripts/generate_driver_wrappers.py"})
         os.cp(path.join(os.scriptdir(), "port", "xmake.lua"), "xmake.lua")
-
         local configs = {}
-        for _, op in ipairs(options) do
-            local v = "n"
-            if package:config(op) ~= false then
-                v = "y"
-            end
-            table.insert(configs, "--"..op.."="..v)
-        end
         import("package.tools.xmake").install(package, configs)
     end)
 
