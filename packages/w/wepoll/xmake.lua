d58db620df7a65ae4c3ed28587153564a03599cd
zeromake <a390720046@gmail.com>
2023-11-04 16:21:42 +0800

    feat: add wepoll pkg

---
+++ packages/w/wepoll/xmake.lua
@@ -0,0 +1,29 @@
+package("wepoll")
+    set_homepage("Todo")
+    set_description("Todo")
+    set_license("MIT")
+    set_urls("https://github.com/piscisaureus/wepoll/archive/refs/tags/v$(version).tar.gz")
+
+    add_versions("1.5.8", "3f68e5f2f5a35c2eb641b73a6d0e605668ea66e5bd9ff1e170660e3b2a60c74e")
+    add_syslinks("ws2_32")
+    on_install(function (package)
+        io.writefile("xmake.lua", [[
+add_rules("mode.debug", "mode.release")
+
+if is_plat("windows") then
+    add_cxflags("/utf-8")
+end
+target("wepoll")
+    set_kind("$(kind)")
+    add_files("wepoll.c")
+    add_headerfiles("wepoll.h")
+    add_defines("WEPOLL_EXPORT=__declspec(dllexport)")
+    add_syslinks("ws2_32")
+]])
+        local configs = {}
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("epoll_create", {includes = {"wepoll.h"}}))
+    end)
