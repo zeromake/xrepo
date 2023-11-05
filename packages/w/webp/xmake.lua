cccc5745f40429df640e523864e70834c713ec1d
zeromake <a390720046@gmail.com>
2023-11-05 15:03:16 +0800

    fix: webp update 1.3.2

--- packages/w/webp/xmake.lua
+++ packages/w/webp/xmake.lua
@@ -4,6 +4,8 @@ package("webp")
 
     set_license("BSD-3")
     set_urls("https://github.com/webmproject/libwebp/archive/refs/tags/v$(version).tar.gz")
+
+    add_versions("1.3.2", "c2c2f521fa468e3c5949ab698c2da410f5dce1c5e99f5ad9e70e0e8446b86505")
     add_versions("1.2.4", "dfe7bff3390cd4958da11e760b65318f0a48c32913e4d5bc5e8d55abaaa2d32e")
 
     add_includedirs("include")
