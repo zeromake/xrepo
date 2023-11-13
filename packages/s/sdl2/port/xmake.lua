b540ee9adbc07b271587df30749e9bdc9c3ddd8a
zeromake <a390720046@gmail.com>
2023-11-14 00:17:37 +0800

    fix: del c++ ld

--- packages/s/sdl2/port/xmake.lua
+++ packages/s/sdl2/port/xmake.lua
@@ -314,7 +314,6 @@ target("sdl2")
         end
     elseif is_plat("android") then
         add_packages("ndk-cpufeatures")
-        add_ldflags("-static-libstdc++")
         add_syslinks("GLESv1_CM", "GLESv2", "OpenSLES", "log", "android")
     end
     add_headerfiles(path.join("include", "*.h"), {prefixdir="SDL2"})
