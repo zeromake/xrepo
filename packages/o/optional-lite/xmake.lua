de9723752a2ea2e6590cb1a4a405867c6fb64fc3
zeromake <a390720046@gmail.com>
2023-12-07 15:43:02 +0800

    feat: add nonstd/optional nonstd/string_view

---
+++ packages/o/optional-lite/xmake.lua
@@ -0,0 +1,15 @@
+package("optional-lite")
+    set_kind("library", {headeronly = true})
+    set_homepage("https://github.com/martinmoene/optional-lite")
+    set_description("optional lite - A C++17-like optional, a nullable object for C++98, C++11 and later in a single-file header-only library")
+    set_license("BSL-1.0")
+    set_urls("https://github.com/martinmoene/optional-lite/archive/refs/tags/v$(version).tar.gz")
+
+    add_versions("3.5.0", "6077cee87e2812afd05a273645051e0b55397a25c220295ddc1d6f49d0cf5cc8")
+    on_install(function (package)
+        os.cp("include/nonstd", package:installdir("include").."/")
+    end)
+
+    -- on_test(function (package)
+    --     assert(package:has_cfuncs("xxx", {includes = {"xx.h"}}))
+    -- end)
---
+++ packages/s/string-view-lite/xmake.lua
@@ -0,0 +1,16 @@
+package("string-view-lite")
+    set_kind("library", {headeronly = true})
+    set_homepage("https://github.com/martinmoene/string-view-lite")
+    set_description("string_view lite - A C++17-like string_view for C++98, C++11 and later in a single-file header-only library")
+    set_license("BSL-1.0")
+    set_urls("https://github.com/martinmoene/string-view-lite/archive/refs/tags/v$(version).tar.gz")
+
+    add_versions("1.7.0", "265eaec08c4555259b46f5b03004dbc0f7206384edfac1cd5a837efaa642e01c")
+    on_install(function (package)
+        os.cp("include/nonstd", package:installdir("include").."/")
+    end)
+
+    -- on_test(function (package)
+    --     assert(package:has_cfuncs("xxx", {includes = {"xx.h"}}))
+    -- end)
+
