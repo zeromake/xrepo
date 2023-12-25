19e19e08aff81c48faba18e611d545087e43252f
zeromake <a390720046@gmail.com>
2023-12-25 10:09:20 +0800

    feat: update nanovg to new version

--- packages/n/nanovg/xmake.lua
+++ packages/n/nanovg/xmake.lua
@@ -1,6 +1,7 @@
 local function getVersion(version)
     local versions ={
-        ["2023.03.29"] = "archive/aa6917c02688ceb72d30fc31f34f0bdfc9b4a559.tar.gz",
+        ["2023.12.25"] = "archive/aa6917c02688ceb72d30fc31f34f0bdfc9b4a559.tar.gz",
+        ["2023.03.29"] = "archive/f45d73db67eaadc3df98971872add86f660a3ee5.tar.gz",
     }
     return versions[tostring(version)]
 end
@@ -20,6 +21,7 @@ package("nanovg")
                 version = getVersion
             }
         )
+        add_versions("2023.12.25", "65d2285824134fb71e03337e4bfaead4a84831e242b5e0d3d9a83cb612250260")
         add_versions("2023.03.29", "852bfeaf9095c6b4550fbde48357c71b323c6c508f97f1025248583d85c8e1dc")
     end
     on_install(function (package)
