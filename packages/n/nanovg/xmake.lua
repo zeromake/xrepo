d2b66fc3df5cab80d0a19c081361b8a41a74d33c
zeromake <a390720046@gmail.com>
2023-12-26 09:22:31 +0800

    fix: update nanovg pkg

--- packages/n/nanovg/xmake.lua
+++ packages/n/nanovg/xmake.lua
@@ -21,8 +21,8 @@ package("nanovg")
                 version = getVersion
             }
         )
-        add_versions("2023.12.25", "65d2285824134fb71e03337e4bfaead4a84831e242b5e0d3d9a83cb612250260")
-        add_versions("2023.03.29", "852bfeaf9095c6b4550fbde48357c71b323c6c508f97f1025248583d85c8e1dc")
+        add_versions("2023.12.25", "852bfeaf9095c6b4550fbde48357c71b323c6c508f97f1025248583d85c8e1dc")
+        add_versions("2023.03.29", "65d2285824134fb71e03337e4bfaead4a84831e242b5e0d3d9a83cb612250260")
     end
     on_install(function (package)
         local configs = {}
