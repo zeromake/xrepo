a9cd7d7ed6b6ab7e607c2d5eaa8d8decdee6a1ef
zeromake <a390720046@gmail.com>
2023-12-27 11:02:31 +0800

    fix: use 2023.12.25

--- packages/n/nanovg/xmake.lua
+++ packages/n/nanovg/xmake.lua
@@ -1,7 +1,7 @@
 local function getVersion(version)
     local versions ={
-        ["2023.12.25"] = "archive/aa6917c02688ceb72d30fc31f34f0bdfc9b4a559.tar.gz",
-        ["2023.03.29"] = "archive/f45d73db67eaadc3df98971872add86f660a3ee5.tar.gz",
+        ["2023.12.25"] = "archive/f45d73db67eaadc3df98971872add86f660a3ee5.tar.gz",
+        ["2023.03.29"] = "archive/aa6917c02688ceb72d30fc31f34f0bdfc9b4a559.tar.gz",
     }
     return versions[tostring(version)]
 end
@@ -21,8 +21,8 @@ package("nanovg")
                 version = getVersion
             }
         )
-        add_versions("2023.12.25", "852bfeaf9095c6b4550fbde48357c71b323c6c508f97f1025248583d85c8e1dc")
-        add_versions("2023.03.29", "65d2285824134fb71e03337e4bfaead4a84831e242b5e0d3d9a83cb612250260")
+        add_versions("2023.12.25", "65d2285824134fb71e03337e4bfaead4a84831e242b5e0d3d9a83cb612250260")
+        add_versions("2023.03.29", "852bfeaf9095c6b4550fbde48357c71b323c6c508f97f1025248583d85c8e1dc")
     end
     on_install(function (package)
         local configs = {}
