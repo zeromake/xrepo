fb0c08f83333908d6e422a0ec2f2f6a3cf6fbdf0
zeromake <a390720046@gmail.com>
2024-01-09 11:14:10 +0800

    feat: add tree-sitter pkg

---
+++ packages/t/tree-sitter/xmake.lua
@@ -0,0 +1,24 @@
+package("tree-sitter")
+    set_homepage("https://tree-sitter.github.io/")
+    set_description("An incremental parsing system for programming tools")
+    set_license("MIT")
+    set_urls("https://github.com/tree-sitter/tree-sitter/archive/refs/tags/v$(version).tar.gz")
+
+    add_versions("0.20.8", "6181ede0b7470bfca37e293e7d5dc1d16469b9485d13f13a605baec4a8b1f791")
+    on_install(function (package)
+        io.writefile('xmake.lua', [[
+add_rules("mode.debug", "mode.release")
+
+target("tree-sitter")
+    set_kind("$(kind)")
+    add_files("lib/src/lib.c")
+    add_includedirs("lib/src", "lib/include")
+    add_headerfiles("lib/include/tree_sitter/*.h", {prefixdir = "tree_sitter"})
+]], {encoding = "binary"})
+        local configs = {}
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    -- on_test(function (package)
+    --     assert(package:has_cfuncs("xxx", {includes = {"xx.h"}}))
+    -- end)
