2b9eef706c356696625cfaddca84d4d8a050704c
zeromake <a390720046@gmail.com>
2024-03-21 10:20:53 +0800

    feat: add ssh2 pkg

---
+++ packages/s/ssh2/port/xmake.lua
@@ -0,0 +1,62 @@
+if xmake.version():gt("2.8.3") then
+    includes("@builtin/check")
+else
+    includes("check_cincludes.lua")
+end
+add_rules("mode.debug", "mode.release")
+
+if is_plat("windows") then
+    add_cxflags("/utf-8")
+end
+
+add_requires("libressl")
+
+function configvar_check_csymbol_exists(define_name, var_name, opt) 
+    configvar_check_csnippets(define_name, 'void* a = (void*)'..var_name..';', opt)
+end
+
+configvar_check_cincludes("HAVE_UNISTD_H", "unistd.h")
+configvar_check_cincludes("HAVE_INTTYPES_H", "inttypes.h")
+configvar_check_cincludes("HAVE_SYS_SELECT_H", "sys/select.h")
+configvar_check_cincludes("HAVE_SYS_UIO_H", "sys/uio.h")
+configvar_check_cincludes("HAVE_SYS_SOCKET_H", "sys/socket.h")
+configvar_check_cincludes("HAVE_SYS_IOCTL_H", "sys/ioctl.h")
+configvar_check_cincludes("HAVE_SYS_TIME_H", "sys/time.h")
+configvar_check_cincludes("HAVE_SYS_UN_H", "sys/un.h")
+configvar_check_cincludes("HAVE_SYS_PARAM_H", "sys/param.h")
+configvar_check_cincludes("HAVE_ARPA_INET_H", "arpa/inet.h")
+configvar_check_cincludes("HAVE_NETINET_IN_H", "netinet/in.h")
+
+configvar_check_cfuncs("HAVE_GETTIMEOFDAY", "gettimeofday", {includes = "sys/types.h"})
+configvar_check_cfuncs("HAVE_STRTOLL", "strtoll", {includes = "stdlib.h"})
+configvar_check_cfuncs("HAVE_STRTOI64", "_strtoi64", {includes = "stdlib.h"})
+configvar_check_cfuncs("HAVE_SNPRINTF", "snprintf", {includes = "stdio.h"})
+configvar_check_cfuncs("HAVE_EXPLICIT_BZERO", "explicit_bzero", {includes = "string.h"})
+configvar_check_cfuncs("HAVE_EXPLICIT_MEMSET", "explicit_memset", {includes = "string.h"})
+configvar_check_cfuncs("HAVE_MEMSET_S", "memset_s", {includes = "string.h"})
+
+configvar_check_cincludes("HAVE_POLL", "poll", {includes = {"poll.h"}})
+configvar_check_cincludes("HAVE_SELECT", "select", {includes = {"sys/select.h"}})
+
+configvar_check_csymbol_exists("HAVE_O_NONBLOCK", "O_NONBLOCK", {includes = {"fcntl.h"}})
+configvar_check_csymbol_exists("HAVE_FIONBIO", "FIONBIO", {includes = {"sys/ioctl.h"}})
+configvar_check_csnippets("HAVE_IOCTLSOCKET_CASE", [[
+#include <sys/ioctl.h>
+int main() {
+    int socket;
+    int flags = IoctlSocket(socket, FIONBIO, (long)1);
+    return 0;
+}]])
+configvar_check_csymbol_exists("HAVE_SO_NONBLOCK", "SO_NONBLOCK", {includes = {"socket.h"}})
+if not is_plat("windows", "mingw") then
+    set_configvar("LIBSSH2_API", "__attribute__ ((__visibility__ (\"default\")))")
+end
+
+target("ssh2")
+    set_kind("$(kind)")
+    add_configfiles("src/libssh2_config.h.in")
+    add_includedirs("$(buildir)", "include")
+    add_headerfiles("include/*.h")
+    add_files("src/*.c")
+    add_packages("libressl")
+    add_defines("HAVE_CONFIG_H","LIBSSH2_OPENSSL")
---
+++ packages/s/ssh2/xmake.lua
@@ -0,0 +1,30 @@
+package("ssh2")
+    set_homepage("https://libssh2.org")
+    set_description("libssh2 is a client-side C library implementing the SSH2 protocol")
+    set_license("BSD-3-Clause")
+    set_urls("https://github.com/libssh2/libssh2/releases/download/libssh2-1.11.0/libssh2-1.11.0.tar.gz")
+
+    add_versions("1.11.0", "3736161e41e2693324deb38c26cfdc3efe6209d634ba4258db1cecff6a5ad461")
+    on_install(function (package)
+        local transforme_configfile = function (input, output) 
+            output = output or input
+            local lines = io.readfile(input):gsub("@([%w_]+)@", "${%1}"):split("\n")
+            local out = io.open(output, 'wb')
+            for _, line in ipairs(lines) do
+                if line:startswith("#cmakedefine") then
+                    line = "${define "..line:split("%s+")[2].."}"
+                end
+                out:write(line)
+                out:write("\n")
+            end
+            out:close()
+        end
+        transforme_configfile("src/libssh2_config_cmake.h.in", "src/libssh2_config.h.in")
+        os.cp(path.join(os.scriptdir(), "port", "xmake.lua"), "xmake.lua")
+        local configs = {}
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    -- on_test(function (package)
+    --     assert(package:has_cfuncs("xxx", {includes = {"xx.h"}}))
+    -- end)
