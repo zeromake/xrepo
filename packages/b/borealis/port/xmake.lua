8d7ea1090e85ac2deb4348ada639307c3bf88fb4
zeromake <a390720046@gmail.com>
2024-04-25 09:55:50 +0800

    feat: add borealis

---
+++ packages/b/borealis/port/xmake.lua
@@ -0,0 +1,122 @@
+add_rules("mode.debug", "mode.release")
+
+option("platfrom")
+    set_default("desktop")
+    set_showmenu(true)
+option_end()
+
+option("window")
+    set_default("glfw")
+    set_showmenu(true)
+option_end()
+
+option("driver")
+    set_default("opengl")
+    set_showmenu(true)
+option_end()
+
+option("winrt")
+    set_default(false)
+    set_showmenu(true)
+option_end()
+
+option("unity_build")
+    set_default(false)
+    set_showmenu(true)
+option_end()
+
+if is_plat("windows") then
+    set_languages("c++20")
+    add_cxflags("/utf-8")
+    add_includedirs("library/include/compat")
+    if is_mode("release") then
+        set_optimize("faster")
+    end
+else
+    set_languages("c++17")
+end
+if is_plat("mingw") then
+    add_defines("WINVER=0x0605")
+end
+
+add_requires("tinyxml2")
+add_requires("yoga =2.0.1")
+add_requires("stb")
+add_requires("nanovg")
+add_requires("nlohmann_json")
+add_requires("fmt")
+add_requires("tweeny")
+
+add_defines(
+    'BRLS_RESOURCES="./resources/"',
+    "YG_ENABLE_EVENTS"
+)
+
+if get_config("driver") == "opengl" then
+    add_requires("glad")
+end
+
+if get_config("window") == "sdl" then
+    if get_config("winrt") then
+        add_requires("sdl2", {configs={shared=true,winrt=true}})
+        add_requires("cppwinrt")
+    else
+        add_requires("sdl2")
+    end
+elseif get_config("window") == "glfw" then
+    add_requires("xfangfang_glfw")
+end
+
+target("borealis")
+    set_kind("static")
+    -- set_kind("shared")
+    add_includedirs("library/include")
+    add_includedirs("library/include/borealis/extern")
+    add_files("library/lib/core/**.cpp")
+    add_files("library/lib/views/**.cpp")
+    add_files("library/lib/extern/libretro-common/**.c")
+
+    if get_config("window") == "glfw" then
+        add_files("library/lib/platforms/glfw/*.cpp")
+        add_files("library/lib/platforms/desktop/*.cpp")
+        add_packages("xfangfang_glfw")
+        add_defines("__GLFW__")
+    elseif get_config("window") == "sdl" then
+        add_files("library/lib/platforms/sdl/*.cpp")
+        add_files("library/lib/platforms/desktop/*.cpp")
+        add_packages("sdl2")
+        if get_config("winrt") then
+            add_defines("SDL_VIDEO_DRIVER_WINRT")
+            add_packages("cppwinrt")
+        end
+        add_defines("__SDL2__")
+    end
+    local driver = get_config("driver")
+    if driver == "metal" then
+        add_defines("BOREALIS_USE_METAL")
+        add_frameworks("Metal", "MetalKit", "QuartzCore")
+        add_files("library/lib/platforms/glfw/driver/metal.mm")
+        add_links("nanovg_metal")
+    elseif driver == "opengl" then
+        add_defines("BOREALIS_USE_OPENGL")
+        add_packages("glad")
+    elseif driver == "d3d11" then
+        add_files("library/lib/platforms/driver/d3d11.cpp", {unity_group = "d3d11"})
+        if get_config("winrt") then
+            add_defines("__WINRT__=1")
+        end
+        add_defines("BOREALIS_USE_D3D11")
+        add_syslinks("d3d11")
+        -- d3d11 可以开启可变帧率
+        -- add_defines("__ALLOW_TEARING__=1")
+    end
+    add_packages("tinyxml2", "nlohmann_json", "nanovg", "fmt", "tweeny", "yoga", "stb")
+    add_defines("BOREALIS_USE_STD_THREAD")
+    if is_plat("macosx") then
+        add_frameworks("SystemConfiguration", "CoreWlan")
+        add_files("library/lib/platforms/desktop/desktop_darwin.mm")
+    end
+    if get_config("unity_build") then
+        add_rules("c++.unity_build", {batchsize = 16})
+        add_rules("c.unity_build", {batchsize = 16})
+    end
---
+++ packages/b/borealis/xmake.lua
@@ -0,0 +1,76 @@
+local function getVersion(version)
+    local versions ={
+        ["2024.04.23-alpha"] = "archive/2c76f6bf1cd0b0d375d617de62ba9ada93e09457.tar.gz",
+    }
+    return versions[tostring(version)]
+end
+
+package("borealis")
+    set_homepage("https://github.com/xfangfang/borealis")
+    set_description("Hardware accelerated, Nintendo Switch inspired UI library for PC, Android, iOS, PSV, PS4 and Nintendo Switch")
+    set_license("Apache-2.0")
+
+    set_urls("https://github.com/xfangfang/borealis/$(version)", {
+        version = getVersion
+    })
+    add_versions("2024.04.23-alpha", "e9a7b2e7b90871b76f5b2133f90052032bda4002eae424706d083c9d40a02772")
+
+    add_configs("window", {description = "use window lib", default = "glfw", type = "string"})
+    add_configs("driver", {description = "use driver lib", default = "opengl", type = "string"})
+    add_configs("winrt", {description = "use winrt api", default = false, type = "boolean"})
+    add_deps(
+        "nanovg",
+        "yoga =2.0.1",
+        "nlohmann_json",
+        "fmt",
+        "tweeny",
+        "stb",
+        "tinyxml2"
+    )
+    add_includedirs("include")
+    if is_plat("windows") then
+        add_includedirs("include/compat")
+        add_syslinks("wlanapi", "iphlpapi", "ws2_32")
+    end
+    on_load(function (package)
+        local window = package:config("window")
+        local driver = package:config("driver")
+        local winrt = package:config("winrt")
+        if window == "glfw" then
+            package:add("deps", "xfangfang_glfw")
+        elseif window == "sdl" then
+            package:add("deps", "sdl2")
+        end
+        if driver == "opengl" then
+            package:add("deps", "glad")
+        elseif driver == "d3d11" then
+            package:add("syslinks", "d3d11")
+        end
+        if winrt then
+            package:add("syslinks", "windowsapp")
+        end
+    end)
+    on_install(function (package)
+        os.cp(path.join(os.scriptdir(), "port", "xmake.lua"), "xmake.lua")
+        local configs = {}
+        configs["window"] = package:config("window")
+        configs["driver"] = package:config("driver")
+        configs["winrt"] = package:config("winrt") and "y" or "n"
+        import("package.tools.xmake").install(package, configs)
+        os.cp("library/include/*", package:installdir("include").."/")
+        os.rm(package:installdir("include/borealis/extern"))
+        os.cp("library/include/borealis/extern/libretro-common", package:installdir("include").."/")
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include <borealis.hpp>
+
+            static void test() {
+                volatile void* i = (void*)&brls::Application::init;
+                if (i) {};
+            }
+        ]]}, {
+            configs = {languages = "c++20", defines = { "BRLS_RESOURCES=\".\"" }},
+        }))
+    end)
