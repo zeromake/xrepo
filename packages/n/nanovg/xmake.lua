f455648b98e9a9a920015a5016c3543c9f95fa98
zeromake <a390720046@gmail.com>
2024-04-25 10:34:48 +0800

    feat: update nanovg

--- packages/n/nanovg/xmake.lua
+++ packages/n/nanovg/xmake.lua
@@ -2,7 +2,7 @@ local function getVersion(version)
     local versions ={
         ["2023.12.25-alpha"] = "archive/f45d73db67eaadc3df98971872add86f660a3ee5.tar.gz",
         ["2023.03.29-alpha"] = "archive/aa6917c02688ceb72d30fc31f34f0bdfc9b4a559.tar.gz",
-        ["2024.04.25-alpha"] = "archive/002881af4804e58d8d980805186999d8c61a26a1.tar.gz",
+        ["2024.04.25-alpha"] = "archive/12335ea226214440296927c8fe821dba9ee17d5a.tar.gz",
     }
     return versions[tostring(version)]
 end
@@ -21,7 +21,7 @@ package("nanovg")
                 version = getVersion
             }
         )
-        add_versions("2024.04.25-alpha", "ebca1482a7a2db77df9714caf9a766df4e5167d524aceab1eb3650df6cfb7893")
+        add_versions("2024.04.25-alpha", "ac08fe1f8b0b79688c16fad2bcf27f711b7ed649d5b13e76069f4137491faff0")
         add_versions("2023.12.25-alpha", "65d2285824134fb71e03337e4bfaead4a84831e242b5e0d3d9a83cb612250260")
         add_versions("2023.03.29-alpha", "852bfeaf9095c6b4550fbde48357c71b323c6c508f97f1025248583d85c8e1dc")
     end
