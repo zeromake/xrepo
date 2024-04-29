987f52429cbe063a791058998ea6801162c5a39d
zeromake <a390720046@gmail.com>
2024-04-29 09:28:44 +0800

    feat: add wgsl support

--- packages/s/sokol-shdc/rules/shader.lua
+++ packages/s/sokol-shdc/rules/shader.lua
@@ -15,7 +15,7 @@ rule("shader")
         batchcmds:vrunv(sokolshdc.program, {
             "--ifdef",
             "-l",
-            "hlsl5:glsl330:glsl300es:metal_macos:metal_ios",
+            "hlsl5:glsl330:glsl300es:metal_macos:metal_ios:metal_sim:wgsl",
             "--input",
             sourcefile,
             "--output",
