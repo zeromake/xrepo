0a2b188eb4515fa7e1a88e9d9990a5a0646ceb05
zeromake <a390720046@gmail.com>
2024-07-04 10:34:03 +0800

    feat: add rapidxml

---
+++ packages/r/rapidxml/xmake.lua
@@ -0,0 +1,23 @@
+local function getVersion(version)
+    local versions ={
+        ["2024.07.01-alpha"] = "archive/4dd86943ce0fa8e550cfa10f40a9082d1a5ad133.tar.gz",
+    }
+    return versions[tostring(version)]
+end
+package("rapidxml")
+    set_kind("library", {headeronly = true})
+    set_homepage("https://github.com/dwd/rapidxml")
+    set_description("RapidXML fork; XML namespacing, per-element parsing, etc ")
+    set_license("MIT")
+    set_urls("https://github.com/dwd/rapidxml/$(version)", {
+        version = getVersion
+    })
+
+    add_versions("2024.07.01-alpha", "e90d4b22adacf5f995e0c785c0ee61fd5c3315553c7157822afb9d85bf28dd0b")
+    on_install(function (package)
+        os.cp("*.hpp", package:installdir("include"))
+    end)
+
+    -- on_test(function (package)
+    --     assert(package:has_cfuncs("xxx", {includes = {"xx.h"}}))
+    -- end)
