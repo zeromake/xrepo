fbbc5c0bfab6a27197323269a42d86143cce429d
zeromake <a390720046@gamil.com>
2024-08-18 10:53:58 +0800

    feat: add sokol wayland

--- packages/s/sokol-wayland/xmake.lua
+++ packages/s/sokol-wayland/xmake.lua
@@ -1,25 +1,21 @@
 local function getVersion(version)
     local versions ={
-        ["2024.06.17-alpha"] = "archive/c970b0bb0768724f5be2ee7ee36bc07b01fc6a62.tar.gz",
-        ["2024.07.02-alpha"] = "archive/78f07444c249deb47c7e6279b556c9dcf94d1cc7.tar.gz",
-        ["2024.07.10-alpha"] = "archive/7b20c1936229370277d1c61bde950bce194de584.tar.gz",
+        ["2024.07.24-alpha"] = "archive/cf7438bd7865f0fc98a5aee8526308fcaa398926.tar.gz",
     }
     return versions[tostring(version)]
 end
 
-package("sokol")
+package("sokol-wayland")
     set_kind("library", {headeronly = true})
     set_homepage("https://github.com/floooh/sokol")
     set_description("Simple STB-style cross-platform libraries for C and C++, written in C.")
     set_license("zlib")
 
-    set_urls("https://github.com/floooh/sokol/$(version)", {
+    set_urls("https://github.com/digitalsignalperson/sokol-custom/$(version)", {
         version = getVersion
     })
 
-    add_versions("2024.07.10-alpha", "e157aab728f7f32c70254c2f9df21f8428e794769081d2758bb8c3dcfdaa8a4f")
-    add_versions("2024.07.02-alpha", "7cd4561c85c2d0f50b09006934d2cf2e5533d81e9e5f4922caedc23aa9717a8d")
-    add_versions("2024.06.17-alpha", "de9c9c1263521927db4d2463dc1ff5ff0100b4a6e8fe60ce32a726a4af5d14f1")
+    add_versions("2024.07.24-alpha", "de229e1065ef4ef8dcd9119f67ad2d88a30d8bca38faa76f2c70a338cb4ccdfc")
 
     on_install(function (package)
         os.cp("*.h", package:installdir("include"))
