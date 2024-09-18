e93348e29fc041642ff58144cd6b2723710a6504
zeromake <a390720046@gmail.com>
2024-09-18 10:32:00 +0800

    fix: ffmpeg use 2024-09-17

--- packages/f/ffmpeg/xmake.lua
+++ packages/f/ffmpeg/xmake.lua
@@ -4,8 +4,8 @@ package("ffmpeg")
     set_license("GPL")
 
     if is_plat("windows") then
-        set_urls("https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n7.0-latest-win64-gpl-shared-7.0.zip")
-        add_versions("latest", "01ba93c050a29559dd0bbd686ad5a952a1d3aba3754e799265f074e4dc251b7c")
+        set_urls("https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2024-09-17-18-11/ffmpeg-n7.0.2-17-gf705bc5b73-win64-gpl-shared-7.0.zip")
+        add_versions("latest", "ba968eeff56802e397b47d4fce0cbfe5296aea0e61fb35ed1f3ec7fcae7e20d8")
     end
     on_install(function (package)
         os.cp("bin/*", package:installdir("bin"))
