bbfb68d366d6fcba393bf7514bd15f68de93257c
zeromake <a390720046@gmail.com>
2024-10-16 00:25:14 +0800

    feat: ffmpeg

--- packages/f/ffmpeg/xmake.lua
+++ packages/f/ffmpeg/xmake.lua
@@ -4,11 +4,12 @@ package("ffmpeg")
     set_license("GPL")
 
     if is_plat("windows") then
-        set_urls("https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2024-09-17-18-11/ffmpeg-n7.0.2-17-gf705bc5b73-win64-gpl-shared-7.0.zip")
-        add_versions("latest", "ba968eeff56802e397b47d4fce0cbfe5296aea0e61fb35ed1f3ec7fcae7e20d8")
+        set_urls("https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2024-10-01-12-55/ffmpeg-n7.1-1-g12682eba2e-win64-gpl-shared-7.1.zip")
+        add_versions("latest", "6b216295a1ec4b4b7b7c55f1760d8601c54b2c7ef62f6324f9096ba7901ae0e0")
     end
     on_install(function (package)
-        os.cp("bin/*", package:installdir("bin"))
+        os.cp("bin/*.exe", package:installdir("bin"))
+        os.cp("bin/*.dll", package:installdir("bin"))
         os.cp("lib/*.lib", package:installdir("lib"))
         os.cp("include/*", package:installdir("include"))
     end)
