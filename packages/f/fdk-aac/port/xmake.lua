b78723767ae65f453e29eedcfe08c3818323b41f
zeromake <a390720046@gmail.com>
2024-11-11 21:06:57 +0800

    fix: fdk-aac support

--- packages/f/fdk-aac/port/xmake.lua
+++ packages/f/fdk-aac/port/xmake.lua
@@ -6,64 +6,34 @@ if is_plat("windows", "mingw") then
     add_defines("UNICODE", "_UNICODE")
 end
 
+local dirs = {
+    "libAACdec",
+    "libAACenc",
+    "libPCMutils",
+    "libFDK",
+    "libSYS",
+    "libMpegTPDec",
+    "libMpegTPEnc",
+    "libSBRdec",
+    "libSBRenc",
+    "libArithCoding",
+    "libDRCdec",
+    "libSACdec",
+    "libSACenc",
+}
+
 set_encodings("utf-8")
 add_requires("zeromake.rules")
 
 target("fdk-aac")
     set_kind("$(kind)")
-    add_rules("@zeromake.rules/export_symbol", {export = {
-        "aacDecoder_AncDataGet",
-        "aacDecoder_AncDataInit",
-        "aacDecoder_Close",
-        "aacDecoder_ConfigRaw",
-        "aacDecoder_DecodeFrame",
-        "aacDecoder_Fill",
-        "aacDecoder_GetFreeBytes",
-        "aacDecoder_GetLibInfo",
-        "aacDecoder_GetStreamInfo",
-        "aacDecoder_Open",
-        "aacDecoder_SetParam",
-        "aacEncClose",
-        "aacEncEncode",
-        "aacEncGetLibInfo",
-        "aacEncInfo",
-        "aacEncOpen",
-        "aacEncoder_GetParam",
-        "aacEncoder_SetParam",
-    }})
-    add_includedirs(
-        "libAACdec/include",
-        "libAACenc/include",
-        "libArithCoding/include",
-        "libDRCdec/include",
-        "libFDK/include",
-        "libMpegTPDec/include",
-        "libMpegTPEnc/include",
-        "libPCMutils/include",
-        "libSACdec/include",
-        "libSACenc/include",
-        "libSBRdec/include",
-        "libSBRenc/include",
-        "libSYS/include"
-    )
+    for _, dir in ipairs(dirs) do
+        add_files(path.join(dir, "src/*.cpp"))
+        add_includedirs(path.join(dir, "include"))
+    end
     add_headerfiles(
         "libAACdec/include/*.h",
         "libAACenc/include/*.h",
         "libSYS/include/*.h",
         {prefixdir = "fdk-aac"}
-    )
-    add_files(
-        "libAACdec/src/*.cpp",
-        "libAACenc/src/*.cpp",
-        "libArithCoding/src/*.cpp",
-        "libDRCdec/src/*.cpp",
-        "libFDK/src/*.cpp",
-        "libMpegTPDec/src/*.cpp",
-        "libMpegTPEnc/src/*.cpp",
-        "libPCMutils/src/*.cpp",
-        "libSACdec/src/*.cpp",
-        "libSACenc/src/*.cpp",
-        "libSBRdec/src/*.cpp",
-        "libSBRenc/src/*.cpp",
-        "libSYS/src/*.cpp"
     )
--- packages/f/fdk-aac/xmake.lua
+++ packages/f/fdk-aac/xmake.lua
@@ -1,20 +1,20 @@
 local function getVersion(version)
     local versions ={
-        ["2024.08.26-alpha"] = "archive/3abe2707606ee66e43cbc78c970a753dbfc4986c.tar.gz",
+        ["2024.10.03-alpha"] = "archive/a76a28900a5349425e7640f65a839a71eb2f27ec.tar.gz",
         --insert getVersion
     }
     return versions[tostring(version)]
 end
 
 package("fdk-aac")
-    set_homepage("https://github.com/zeromake/fdk-aac")
+    set_homepage("https://android.googlesource.com/platform/external/aac")
     set_description("A standalone library of the Fraunhofer FDK AAC code from Android.")
     set_license("MIT")
-    set_urls("https://github.com/zeromake/fdk-acc/$(version)", {version = getVersion})
+    set_urls("https://github.com/zeromake/fdk-aac/$(version)", {version = getVersion})
 
     --insert version
-    add_versions("2024.08.26-alpha", "67ce3b8dee444990e6494a2711c4b3a552fe53e21f2ef6a51ffd3877a70f29f4")
-    add_patches("2024.08.26-alpha", path.join(os.scriptdir(), "patches/fix.patch"), "9deea84d11cd43b27b4db1c453c5fd5cbfd606b3020000b6d39131fc9773964b")
+    add_versions("2024.10.03-alpha", "d3abfe729a4588d58638439125728f2b4da124331b13747d5deede420a17bef2")
+    add_patches("2024.10.03-alpha", path.join(os.scriptdir(), "patches/fix.patch"), "9deea84d11cd43b27b4db1c453c5fd5cbfd606b3020000b6d39131fc9773964b")
     on_install(function (package)
         os.cp(path.join(os.scriptdir(), "port", "xmake.lua"), "xmake.lua")
         local configs = {}
