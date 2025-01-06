f4284c0bd23df107710af269d1c6bda3d4645926
zeromake <a390720046@gmail.com>
2025-01-07 00:56:30 +0800

    feat: add gettext-tools support

---
+++ packages/g/gettext-tools/xmake.lua
@@ -0,0 +1,12 @@
+package("gettext-tools")
+    set_kind("binary")
+    set_homepage("https://github.com/vslavik/gettext-tools-windows")
+    set_description("GNU gettext tools compiled binaries for Windows")
+    set_license("GPL-3.0")
+    set_urls("https://github.com/vslavik/gettext-tools-windows/releases/download/v$(version)/gettext-tools-windows-$(version).zip")
+
+    --insert version
+    add_versions("0.23", "7242cc9caee95fb3e2dbbf7ee0fbdcea00ed1024256e54bfea4d52275605d7a7")
+    on_install(function (package)
+        os.cp("*", package:installdir())
+    end)
