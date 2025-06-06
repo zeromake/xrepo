e8dc2ff1d31ca33e1dda27a048995c86dede6564
zeromake <a390720046@gmail.com>
2025-06-06 11:40:08 +0800

    fix: fwlib_stc8 use remote sdcc

--- packages/f/fwlib_stc8/port/xmake.lua
+++ packages/f/fwlib_stc8/port/xmake.lua
@@ -1,7 +1,7 @@
 add_rules("mode.debug", "mode.release")
 
 set_encodings("utf-8")
-add_requires("zeromake.rules")
+add_requires("sdcc", "zeromake.rules")
 
 option("model")
     set_default("STC8H1K08")
@@ -11,6 +11,8 @@ option_end()
 
 target("fwlib_stc8")
     set_kind("$(kind)")
+    add_packages("sdcc", "zeromake.rules")
+    set_toolchains("@sdcc")
     add_rules("@zeromake.rules/mcs51", {model = get_config("model")})
     add_files("src/*.c")
     add_includedirs("include")
